e8a09e1997f0edc5395765b8856cd7d1aa35b8fb
unknown <klohiri.toure22@gmail.com>
2026-06-04 02:25:12 +0000

    Correction de la synchronisation et du typage UUID dans LampadaireCard

--- supabase/supabaseClient.js
+++ supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cbyrffmyignbhikgoaov.supabase.co/rest/v1/lampadaires';
+const supabaseUrl = 'https://cbyrffmyignbhikgoaov.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNieXJmZm15aWduYmhpa2dvYW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDc3NjEsImV4cCI6MjA4MTcyMzc2MX0.PD8CtM97wjHpIUpNEP5I6IcxPLl3VfOt58c5MRcknCo';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
